1b49f5e4f596110e5e1580efc9f7047ac9243dea
Matthew Wollenweber <mjw@insomniac.technology>
2016-04-22 16:34:01 -0700

    errant files

--- app/services/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from flask import Blueprint
-
-alexa = Blueprint('alexa', __name__)
-
-from . import views
--- app/services/config.py
+++
@@ -1,3 +0,0 @@
-
-from app.database import SQLALCHEMY_DATABASE_URI
-url = "http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
